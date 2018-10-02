1d84555383237cf96d174e3604f4172b9f9f6aae
Jeremy Emata <jeremy.emata@gmail.com>
2018-10-02 11:30:28 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,12 @@
 class Book 
 
 attr_accessor :title, author, page_count, genre 
-
-def initialize(title)
-  @title = title 
-end 
-
+    
+  def initialize(title)
+    @title = title 
+  end 
+      
+  def turn_page
+  end 
 
 end 
